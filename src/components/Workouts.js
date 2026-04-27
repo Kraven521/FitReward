164c65b49583727b2b720423fb9c78ff56930aee
Kraven521 <njorogevictor521@gmail.com>
2026-04-27 18:09:25 +0300

    Updated Workouts.js to correct ad slots and removed HTML script code.

---
+++ src/components/Workouts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Workouts = () => {
+  return (
+    <div>
+      {/* Other components */}
+      <GoogleAd adSlot={3463472593} />
+      {/* Other components */}
+    </div>
+  );
+};
+
+export default Workouts;
